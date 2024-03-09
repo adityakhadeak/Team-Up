0b8c9d5e1e44a97030f29eba27e4537fd8319c32
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-03-10 00:28:10 +0530

    Initial server and database setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectToDB from './server/database/db.js'
+
+const app=express()
+
+dotenv.config()
+
+const port=process.env.PORT || 4000
+
+connectToDB()
+
+app.listen(port,()=>{
+    console.log(`Server connected on port ${port}`)
+})
