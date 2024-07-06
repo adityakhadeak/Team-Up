b9aee79945dd2f54d536f1d58b824009545b157f
Sahilchaudhari-746 <sahil21comp@student.mes.ac.in>
2024-07-06 11:23:30 +0530

    Postproject save in database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import 'dotenv/config'; // Ensure this line is at the top
 import cors from 'cors'
 import connectToDB from './database/db.js'
 import authRouter from './routes/authRoutes.js'
 import userRouter from './routes/userRoute.js'
+import projectRouter from './routes/Project.js'
 const app=express()
 
 dotenv.config()
@@ -12,6 +14,7 @@ app.use(express.json())
 
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
+app.use('/projects',projectRouter)
 const port=process.env.PORT || 4000
 
 connectToDB()
